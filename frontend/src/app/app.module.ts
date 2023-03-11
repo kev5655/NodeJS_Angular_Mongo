0df3830fda79d889d46142ade8f631102b84c997
Kevin Zahn <kevin.zahn@bluewin.ch>
2023-03-11 16:42:09 +0100

    add authentication functionality

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,6 +15,10 @@ import { TitleComponent } from './components/partials/title/title.component';
 import { NotFoundComponent } from './components/partials/not-found/not-found.component';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import {ToastrModule} from "ngx-toastr";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { InputContainerComponent } from './components/partials/input-container/input-container.component';
+import { InputValidationComponent } from './components/partials/input-validation/input-validation.component';
 
 @NgModule({
   declarations: [
@@ -27,14 +31,22 @@ import {ReactiveFormsModule} from "@angular/forms";
     CartPageComponent,
     TitleComponent,
     NotFoundComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    InputContainerComponent,
+    InputValidationComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     RatingModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ToastrModule.forRoot({
+      timeOut:3000,
+      positionClass: 'toast-bottom-right',
+      newestOnTop: false
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/partials/header/header.component.ts
+++ frontend/src/app/components/partials/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {CartService} from "../../../services/cart.service";
+import {UserService} from "../../../services/user.service";
+import {User} from "../../../shared/models/User";
 
 @Component({
   selector: 'app-header',
@@ -8,9 +10,22 @@ import {CartService} from "../../../services/cart.service";
 })
 export class HeaderComponent {
   cartQuantity=0;
-  constructor(cardService:CartService) {
+  user!:User;
+  constructor(cardService:CartService,private userService:UserService) {
     cardService.getCartObservable().subscribe((newCart) => {
       this.cartQuantity = newCart.totalCount;
     })
+
+    userService.userObservable.subscribe((newUser) => {
+      this.user = newUser;
+    })
+  }
+
+  logout(){
+    this.userService.logout();
+  }
+
+  get isAuth(){
+    return this.user.token;
   }
 }
