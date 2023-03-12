b774dac51142b3546c33dad8941e8361d8c4186f
Kevin Zahn <kevin.zahn@bluewin.ch>
2023-03-12 12:25:45 +0100

    add checkout page and order items module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { RegisterPageComponent } from './components/pages/register-page/register
 import { LoadingComponent } from './components/partials/loading/loading.component';
 import {LoadingInterceptor} from "./shared/interceptors/loading.interceptor";
 import { CheckoutPageComponent } from './components/pages/checkout-page/checkout-page.component';
+import { OrderItemsListComponent } from './components/partials/order-itmes-list/order-items-list.component';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,8 @@ import { CheckoutPageComponent } from './components/pages/checkout-page/checkout
     DefaultButtonComponent,
     RegisterPageComponent,
     LoadingComponent,
-    CheckoutPageComponent
+    CheckoutPageComponent,
+    OrderItemsListComponent
   ],
   imports: [
     BrowserModule,
