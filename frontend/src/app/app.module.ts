62a6b1da1ac41fe253f6b54eb3a02dcec294cd03
Kevin Zahn <kevin.zahn@bluewin.ch>
2023-03-12 11:26:44 +0100

    add loading module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from "@angular/common/http";
+import {HttpClientModule, HTTP_INTERCEPTORS} from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,6 +22,9 @@ import { InputValidationComponent } from './components/partials/input-validation
 import { TextInputComponent } from './components/partials/text-input/text-input.component';
 import { DefaultButtonComponent } from './components/partials/default-button/default-button.component';
 import { RegisterPageComponent } from './components/pages/register-page/register-page.component';
+import { LoadingComponent } from './components/partials/loading/loading.component';
+import {LoadingInterceptor} from "./shared/interceptors/loading.interceptor";
+import { CheckoutPageComponent } from './components/pages/checkout-page/checkout-page.component';
 
 @NgModule({
   declarations: [
@@ -39,7 +42,9 @@ import { RegisterPageComponent } from './components/pages/register-page/register
     InputValidationComponent,
     TextInputComponent,
     DefaultButtonComponent,
-    RegisterPageComponent
+    RegisterPageComponent,
+    LoadingComponent,
+    CheckoutPageComponent
   ],
   imports: [
     BrowserModule,
@@ -54,7 +59,9 @@ import { RegisterPageComponent } from './components/pages/register-page/register
       newestOnTop: false
     })
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
